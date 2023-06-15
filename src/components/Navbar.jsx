e2eeb130c078200203251d63fc44c20a8e2d4f83
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-06-16 02:10:51 +0630

    side bar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { BiSearch } from "react-icons/bi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className=" bgTransparent flex py-5 gap-9 items-center">
+      <p className=" title text-2xl ms-5">
+        <RxHamburgerMenu />
+      </p>
+      <div className=" flex bg-[#ffffff33] w-[450px] items-center justify-between py-2 px-3 rounded">
+        <input
+          type="text"
+          className=" bg-transparent text-sm"
+          placeholder="Enter Keywords"
+        />
+        <p className=" text-[#ffffffc2]">
+          <BiSearch />
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,12 +3,8 @@ import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className=" h-screen flex flex-col items-center justify-center ">
+    <div>
       <Sidebar />
-      <div className=" w-[600px] bgTransparent h-[400px] ">
-        <h2 className="  title">Dashboard</h2>
-        <h2 className=" subtitle">subtitle</h2>
-      </div>
     </div>
   );
 };
