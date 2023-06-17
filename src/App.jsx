dffca9920ffc214d89ad334cbcf6701ecda50adf
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-06-17 22:10:32 +0630

    sidebar navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Path from "./route/Path";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div style={{
-      backgroundImage : `url("https://themewagon.github.io/dashtreme/assets/images/bg-themes/1.png")`,
-      backgroundSize: "100% 100%",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-    }} className=" h-screen">
-      <Path />
+    <div
+      style={{
+        backgroundImage: `url("https://themewagon.github.io/dashtreme/assets/images/bg-themes/1.png")`,
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+      className=" h-[700px]">
+      <div className=" flex">
+        <Sidebar isOpen={isSidebarOpen} />
+
+        <Navbar toggleSidebar={toggleSidebar} isOpen={isSidebarOpen} />
+        {/* <Path /> */}
+      </div>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
 import {
   AiOutlineAppstore,
   AiOutlineUnorderedList,
@@ -8,152 +7,100 @@ import {
   AiOutlineLock,
 } from "react-icons/ai";
 import { RiContactsLine } from "react-icons/ri";
-import {
-  MdOutlineKeyboardArrowDown,
-  MdOutlineKeyboardArrowUp,
-} from "react-icons/md";
+import { GiNewspaper } from "react-icons/gi";
 import { CgProfile } from "react-icons/cg";
 import "./sidebar.css";
-import { Link } from "react-router-dom";
 
-const Sidebar = () => {
-  const [open, setOpen] = useState(false);
-  const hundleSidebar = () => {
-    setOpen(!open);
-  };
-
-  const [drop, setDrop] = useState(false);
-  const hundleDrop = () => {
-    setDrop(!drop);
-  };
-
+const Sidebar = ({ isOpen }) => {
   return (
-    <div className=" flex items-start gap-3">
-      {/* Sidebar  */}
-      <div
-        className={`bgTransparent sideBar text-white h-screen ${
-          open ? "block" : "hidden"
-        }`}>
-        {/* logo and name  */}
-        <div className=" flex gap-5 items-center px-10 py-5 bg-black bg-opacity-30">
-          <img
-            src="https://eduvibe.devsvibe.com/main/wp-content/uploads/2023/01/logo-2.png"
-            className=" w-[100px]"
-            alt=""
-          />
-          <p className=" title">Hello</p>
-        </div>
-        <hr className=" bg-slate-500 border-0 py-[0.2px] " />
-        {/* main navigation  */}
-        <div className=" flex flex-col gap-4 mt-4">
-          <p className=" text-[#ffffffa6] text-[12px] uppercase ms-4">
-            main navigation
+    <aside
+      className={` bg-black lg:bg-opacity-20 z-50 bg-opacity-100 w-[230px] flex flex-col fixed ${
+        isOpen ? "" : "hidden"
+      }`}>
+      {/* Sidebar content */}
+      <div className=" flex gap-5 items-center px-10 py-5 h-[65px] bg-black bg-opacity-30 ">
+        <img
+          className=" w-[100px]"
+          src="https://eduvibe.devsvibe.com/main/wp-content/uploads/2023/01/logo-2.png"
+          alt=""
+        />
+      </div>
+      <hr className=" bg-slate-500 border-0 py-[0.2px] " />
+      {/* main navigation  */}
+      <div className=" flex flex-col gap-4 mt-4 pb-[50px]">
+        <p className=" text-[#ffffffa6] text-[12px] uppercase ms-4">
+          main navigation
+        </p>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <AiOutlineAppstore />
           </p>
-          <Link to={"/"}>
-            <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
-              <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-                <AiOutlineAppstore />
-              </p>
-              <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-                Dashboard
-              </span>
-            </div>
-          </Link>
-          <Link to={"/forms"}>
-            <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
-              <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-                <AiOutlineUnorderedList />
-              </p>
-              <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-                Forms
-              </span>
-            </div>
-          </Link>
-          <Link to={"/calendar"}>
-            <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
-              <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-                <AiOutlineCalendar />
-              </p>
-              <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-                Calendar
-              </span>
-            </div>
-          </Link>
-          <Link to={"/profile"}>
-            <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
-              <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-                <RiContactsLine />
-              </p>
-              <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-                Profile
-              </span>
-            </div>
-          </Link>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Dashboard
+          </span>
+        </div>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <AiOutlineUnorderedList />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Forms
+          </span>
+        </div>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <AiOutlineCalendar />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Calendar
+          </span>
+        </div>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <RiContactsLine />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Profile
+          </span>
+        </div>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <AiOutlineLock />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Login
+          </span>
+        </div>
 
-          <Link to={"/login"}>
-            <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
-              <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-                <AiOutlineLock />
-              </p>
-              <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-                Login
-              </span>
-            </div>
-          </Link>
-          <Link to={"/register"}>
-            <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
-              <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-                <CgProfile />
-              </p>
-              <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-                Registration
-              </span>
-            </div>
-          </Link>
-          <div className=" relative group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3  ">
-            <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
-              <AiOutlineTable />
-            </p>
-            <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
-              Tables
-            </span>
-            <button
-              className=" font-bold title text-xl py-5 ps-[40%]"
-              onClick={hundleDrop}>
-              {drop ? (
-                <MdOutlineKeyboardArrowUp />
-              ) : (
-                <MdOutlineKeyboardArrowDown />
-              )}
-            </button>
-            {drop ? (
-              <ul className=" absolute top-16 left-0 w-[100%] ">
-                <li className=" flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2">
-                  <p className=" text-[#ffffffa6] text-xl hover:text-[#ffffff]">
-                    <AiOutlineTable />
-                  </p>
-                  <button className=" text-[#ffffffa6] text-[15px] hover:text-[#ffffff]">
-                    Teacher Tables
-                  </button>
-                </li>
-                <li className=" flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2">
-                  <p className=" text-[#ffffffa6] text-xl hover:text-[#ffffff]">
-                    <AiOutlineTable />
-                  </p>
-                  <button className=" text-[#ffffffa6] text-[15px] hover:text-[#ffffff]">
-                    Student Tables
-                  </button>
-                </li>
-              </ul>
-            ) : null}
-          </div>
+        <hr className=" bg-slate-500 border-0 py-[0.2px] " />
+
+        <p className=" text-[#ffffffa6] text-[12px] uppercase ms-4">Tables</p>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <CgProfile />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Students List
+          </span>
+        </div>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <AiOutlineTable />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Teacher List
+          </span>
+        </div>
+        <div className=" group flex items-center gap-3 hover:bg-[#ffffff33] hover:border-s-2 ps-3 py-2 ">
+          <p className=" text-[#ffffffa6] text-xl group-hover:text-[#ffffff]">
+            <GiNewspaper />
+          </p>
+          <span className=" text-[#ffffffa6] text-[15px] group-hover:text-[#ffffff]">
+            Course List
+          </span>
         </div>
       </div>
-      {/* Burger icon  */}
-      <button className=" font-bold title text-xl py-5" onClick={hundleSidebar}>
-        <RxHamburgerMenu />
-      </button>
-    </div>
+    </aside>
   );
 };
 
