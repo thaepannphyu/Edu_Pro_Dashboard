a168902680de9c9bf27330cce6de8de092062dd1
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-19 19:24:05 +0630

    dashboard-2add

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import LineChart from '../components/dashboard-componentes/LineChart'
+import Progress from '../components/dashboard-componentes/Progress'
 
 const Dashboard = () => {
   return (
     <div className=' h-screen'>
-      <LineChart/>
       <Progress />
+      <LineChart/>
     </div>
   )
 }
