8b9e5cc593113b8fc9cd14e2018c6f0eaf296894
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-19 20:34:22 +0630

    responsive1

--- src/components/dashboard-componentes/LineChart.jsx
+++ src/components/dashboard-componentes/LineChart.jsx
@@ -75,7 +75,7 @@ const LineChart = () => {
         },
       };
   return (
-    <div className=' w-[55%] bgTransparent'>
+    <div className=' w-[55%] max-lg:w-[95%]  bgTransparent'>
         <h1 className=' title font-bold pl-16 py-3'>Title</h1>
         <hr className='border-t border-t-[rgba(255, 255, 255, 0.1)]' />
 
@@ -83,35 +83,35 @@ const LineChart = () => {
             <div className=' subtitle flex gap-2 items-center'>
                 <p><BsCircleFill/></p>
                 <p>New students</p>
-            </div>
+            </div> 
             <div className=' subtitle flex gap-2 items-center'>
                 <p className=' text-[rgba(255, 255, 255, 0.4)]'><BsCircleFill/></p>
                 <p>Old students</p>
             </div>
         </div>
 
-        <div className=' w-full h-full flex justify-center py-5'>
+        <div className=' w-full max-h-[600px] flex justify-center py-5'>
             <Line data={data} options={options}>
             </Line>
         </div>
 
-        <div className=' w-full flex justify-center border-t border-r-[rgba(255, 255, 255, 0.2)] '>
-            <div className=' border-r border-r-[rgba(255, 255, 255, 0.2)] w-full flex justify-center py-3'>
+        <div className=' w-full max-lg:flex-col flex justify-center border-t border-r-[rgba(255, 255, 255, 0.2)] '>
+            <div className=' max-lg:border-r-0 border-r border-r-[rgba(255, 255, 255, 0.2)] w-full flex justify-center py-3'>
                 <div>
                     <h1 className=' text-center text-lg title'>45.87M</h1>
-                    <p className=' text-sm text-[#ffffffd9] flex items-center'>Students< VscArrowSmallUp className=' text-sm font-bold'/>12.56%</p>
+                    <p className=' text-sm text-[#ffffffd9] flex items-center'>Students< VscArrowSmallUp className=' text-2xl font-bold'/>12.56%</p>
                 </div>
             </div>
-            <div className=' border-r border-r-[rgba(255, 255, 255, 0.2)] w-full flex justify-center py-3'>
+            <div className='  max-lg:border-r-0 border-r border-r-[rgba(255, 255, 255, 0.2)] w-full flex justify-center py-3'>
                 <div>
                     <h1 className=' text-center text-lg title'>45.87M</h1>
-                    <p className=' text-sm text-[#ffffffd9] flex items-center'>Students< VscArrowSmallUp className=' text-sm font-bold'/>12.56%</p>
+                    <p className=' text-sm text-[#ffffffd9] flex items-center'>Students< VscArrowSmallUp className=' text-2xl font-bold'/>12.56%</p>
                 </div>
             </div>
             <div className=' w-full flex justify-center py-3'>
                 <div>
                     <h1 className=' text-center text-lg title'>45.87M</h1>
-                    <p className=' text-sm text-[#ffffffd9] flex items-center'>Students< VscArrowSmallUp className=' text-sm font-bold'/>12.56%</p>
+                    <p className=' text-sm text-[#ffffffd9] flex items-center'>Students< VscArrowSmallUp className=' text-2xl font-bold'/>12.56%</p>
                 </div>
             </div>
         </div>
