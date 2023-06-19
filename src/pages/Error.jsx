15081724d76b76ed6e97bbe166a4a8e1c1313ebb
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-06-19 17:38:38 +0630

    error-page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import errorimg from "../../public/errorimg.png";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div className=' h-[700px]'>
-      <img src="" alt="" />
+    <div className=" h-screen ">
+      <div className=" flex flex-col items-center justify-center gap-10 ">
+        <p className=" text-center text-3xl font-extrabold text-[#ffffffea]">
+          Sorry ! Page not found
+        </p>
+        <img
+          className=" skew-y-6 w-72"
+          src={errorimg}
+          alt=""
+        />
+        <Link to={"/"} className=" flex justify-center">
+          <button className=" animate-bounce bg-[#ffffff17] py-2 px-6 rounded shadow-lg font-bold text-[#ffffffe3] tracking-wide  ">
+            Go Back Home
+          </button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
