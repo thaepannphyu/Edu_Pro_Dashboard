4829b3b6013c6bf4e75852fc33bf6551a2620285
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-06-21 10:19:54 +0630

    check8

--- src/components/context/StateContext.jsx
+++ src/components/context/StateContext.jsx
@@ -2,18 +2,12 @@ import { Children, createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
-export const StateContextProvider = ({children}) => {
+export const StateContextProvider = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
 
-    const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const data = { isSidebarOpen, setSidebarOpen };
 
-    const data = {isSidebarOpen,setSidebarOpen};
-
-    return (
-        <StateContext.Provider value={data}>
-            {children}
-        </StateContext.Provider>
-    )
-
-}
+  return <StateContext.Provider value={data}>{children}</StateContext.Provider>;
+};
 
 export const StateContextCustom = () => useContext(StateContext);
