95ce2c5333e838ef4b2c02bb162ffd0ab141c893
thaepannphyu <122225000+thaepannphyu@users.noreply.github.com>
2024-02-29 19:36:24 +0630

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { BsFlagFill } from "react-icons/bs";
 import { IoExitOutline } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import "./navbar.css";
-import Profile from "../assets/profile.png";
+import Profile from "../assets/Profile.png";
 import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
 import { StateContextCustom } from "./context/StateContext";
